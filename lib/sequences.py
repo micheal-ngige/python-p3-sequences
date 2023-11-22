43db8339f201004fdd45f8d96c3e2300f3affa8e
michael <michaelngige@gmail.com>
2023-11-22 12:30:26 +0300

    passing tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib_list.append(a)
+        a, b = b, a + b
+    print(fib_list)
+
+
+print_fibonacci(5)
